e522783d7924ad7f89245b2050b1c343a24b5d89
nathan <Nathan.Rogers@oldcastle.com>
2017-10-12 23:25:13 -0600

    added 3d canvas

--- topDown/draw.js
+++ topDown/draw.js
@@ -3,6 +3,7 @@ function drawLine(context, [fx,fy], [tx,ty], settings){
         context.moveTo(fx,fy)
         context.lineTo(tx,ty)
         context.lineWidth = settings.width
+        context.strokeStyle = settings.color || 'black'
         context.stroke()
         context.closePath()
 }
@@ -39,27 +40,58 @@ function transform(line, [px,py,a]) {
             [[relativeFx, relativeFy], [relativeTx,relativeTy]] = [
             [fx - px, fy - py],
             [tx - px, ty - py]
-            ],
-            rz1 = relativeFx * Math.cos(a) + relativeFy * Math.sin(a),
-            rz2 = relativeTx * Math.cos(a) + relativeTy * Math.sin(a)
-            rx1 = relativeFx * Math.sin(a) - relativeFy * Math.cos(a),
-            rx2 = relativeTx * Math.sin(a) - relativeTy * Math.cos(a)
+        ],
+        rz1 = relativeFx * Math.cos(a) + relativeFy * Math.sin(a),
+        rz2 = relativeTx * Math.cos(a) + relativeTy * Math.sin(a)
+        rx1 = relativeFx * Math.sin(a) - relativeFy * Math.cos(a),
+        rx2 = relativeTx * Math.sin(a) - relativeTy * Math.cos(a)
     return [rz1,rz2,rx1,rx2]
 }
 
 function drawWorld3(world, context, player){
-    world.forEach(line => {
-        const [rz1,rz2,rx1,rx2] = transform(line, player)
-        const x1 = -rx1 * 16 / rz1,
-              y1a = -50 / rz1,
-              y1b =  50 / rz1,
-              x2 = -rx2 * 16 / rz2,
-              y2a = -50 / rz2,
-              y2b = 50 / rz2
-        drawLine(context,[50+x1,50+y1a],[50+x2,50+y2a],{width:1})
-        drawLine(context,[50+x1,50+y1b],[50+x2,50+y2b],{width:1})
-        drawLine(context,[50+x1,50+y1a],[50+x1,50+y1b],{width:1})
-        drawLine(context,[50+x2,50+y2a],[50+x2,50+y2b],{width:1})
+    const colors = ['yellow','green','purple','orange']
+    world.forEach((line,i) => {
+        let [rz1,rz2,rx1,rx2] = transform(line, player)
+        if(rz1 > 0 || rz2 > 0){
+            const [ix1,iz1] = intersect(rx1,rz1,rx2,rz2,-0.0001,0.0001,-20,5)
+            const [ix2,iz2] = intersect(rx1,rz1,rx2,rz2, 0.0001,0.0001, 20,5)
+            if(rz1 <=0) [rx1, rz1] = 
+                iz1 > 0
+                ? [ix1,iz1]
+                : [ix2,iz2]
 
+            if(rz2 <= 0) [rx2,rz2] 
+                = iz1 > 0
+                ? [ix1,iz1]
+                : [ix2, iz2]
+
+            const x1  = -rx1 * 16 / rz1,
+                  y1a =       -50 / rz1,
+                  y1b =        50 / rz1,
+                  x2  = -rx2 * 16 / rz2,
+                  y2a =       -50 / rz2,
+                  y2b =        50 / rz2
+            for(x= x1; x > x2; x-=3){
+                ya = y1a + (x - x1) * (y2a - y1a) / (x2 - x1)
+                yb = y1b + (x - x1) * (y2b - y1b) / (x2 - x1)
+                drawLine(context,[100+x,0],[100+x,100+ -ya],{width:4,color:'gray'})
+                drawLine(context,[100+x,100+yb],[100+x,140],{width:4,color:'blue'})
+                drawLine(context,[100+x,100+ya],[100+x,100+yb],{width:4,color:colors[i%4]})
+            }
+            drawLine(context,[100+x1,100+y1a],[100+x1,100+y1b],{width:1})
+            drawLine(context,[100+x2,100+y2a],[100+x2,100+y2b],{width:1})
+            // drawLine(context,[100+x1,100+y1a],[100+x1,100+y1b],{width:1})
+            // drawLine(context,[100+x2,100+y2a],[100+x2,100+y2b],{width:1})
+        }
     })
+}
+
+const fnCross = (x1,y1,x2,y2) => x1*y2 - x2*y1
+const intersect = (x1,y1,x2,y2,x3,y3,x4,y4,x,y) =>{
+    x = fnCross(x1,y1,x2,y2)
+    y = fnCross(x3,y3,x4,y4)
+    det = fnCross(x1-x2,y1-y2,x3-x4,y3-y4)
+    x = fnCross(x,x1-x2,y,x3-x4) / det
+    y = fnCross(x, y1-y2,y,y3-y4)/det
+    return [x,y]
 }
